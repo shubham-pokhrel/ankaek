82e4bb172f361ca027bbf0951d1d02e6c63d2ae5
Shubham-Pokhrel <contact@shubhampokhrel.com.np>
2025-06-06 23:37:42 +0545

    feat(appConstant): Added App Constants class

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const String postsEndpoint = '/posts';
+  static const String hiveBoxName = 'saved_posts';
+}
